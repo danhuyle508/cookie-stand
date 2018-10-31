a64414dfef41390cd0176ac2be113d0851f29015
Dan Le <Danhuyle508@gmail.com>
2018-10-30 20:08:37 -0700

    table created with all data added

--- app.js
+++ app.js
@@ -1,114 +1,74 @@
-var arrayOfHours=['6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM']
-
-//object to store all arrays that contain cookies sold per hour for each store
-var locationObject = {
-  place1standPike: [],
-  placeSeaTacAirport: [],
-  placeSeattleCenter: [],
-  placeCapitolHill: [],
-  placeAlki: [],
-}
-
-//prints the data to the site
-function printData(id, arr, total){
-  var cookiesElement = document.getElementById(id);
-  for(var i =0; i < 15; i++){
-    var cElement = document.createElement('li');
-    cElement.textContent = arr[i];
-    cookiesElement.appendChild(cElement);
-  }
-  cElement.textContent = `Total: ${total} cookies`;
-  cookiesElement.appendChild(cElement);
-}
-
-//calculates cookies per hour
-function calculateAverageCookiesPerHour(max, min, average, location, id){
-  var sumOfCookies = 0;
-  for(var i = 0; i < 15; i++){
-    var randNum = Math.round(Math.random() * (max - min) + min);
-    var cookiesForHour = Math.round(randNum * average);
-    location.push((`${arrayOfHours[i]}: ${cookiesForHour} cookies.`));
-    sumOfCookies += cookiesForHour;
-  }
-  printData(id, location, sumOfCookies);
-}
-
-var place1standPike = {
-  min:23,
-  max:65,
-  averagePerPerson: 6.3,
-  location: '1st and Pike',
-  cookiesSoldEveryHour:function(){
-    calculateAverageCookiesPerHour(this.max,this.min,this.averagePerPerson, locationObject.place1standPike, 'cookiesCalculatedFor1stAndPike');
-  } 
-}
-place1standPike.cookiesSoldEveryHour();
-
-var placeSeaTacAirport = {
-  min: 3,
-  max: 24,
-  averagePerPerson: 1.2,
-  location: 'SeaTac Airport',
-  cookiesSoldEveryHour:function(){
-    calculateAverageCookiesPerHour(this.max, this.min, this.averagePerPerson, locationObject.placeSeaTacAirport, 'cookiesCalculatedForSeaTacAirport');
-  }
-}
-placeSeaTacAirport.cookiesSoldEveryHour();
-
-var placeSeattleCenter = {
-  min: 11,
-  max: 38,
-  averagePerPerson: 3.7,
-  location: 'Seattle Center',
-  cookiesSoldEveryHour:function(){
-    calculateAverageCookiesPerHour(this.max, this.min, this.averagePerPerson,locationObject.placeSeattleCenter,'cookiesCalculatedForSeattleCenter');
-  }
-}
-placeSeattleCenter.cookiesSoldEveryHour();
-
-var placeCapitolHill ={
-  min: 20,
-  max: 38,
-  averagePerPerson: 2.3,
-  location: 'Capitol Hill',
-  cookiesSoldEveryHour:function(){
-calculateAverageCookiesPerHour(this.max, this.min, this.averagePerPerson, locationObject.placeCapitolHill, 'cookiesCalculatedForCapitolHill')
-  }
-}
-placeCapitolHill.cookiesSoldEveryHour();
-
-var placeAlki ={
-  min: 2,
-  max: 16,
-  averagePerPerson: 4.6,
-  location: 'Alki',
-  cookiesSoldEveryHour: function(){
-    calculateAverageCookiesPerHour(this.max, this.min, this.averagePerPerson, locationObject.placeAlki, 'cookiesCalculatedForAlki')
-  }
-}
-placeAlki.cookiesSoldEveryHour();
-
-function place(min,max,place,average,hoursOfOperation,id){
+var stores = [];
+var hours =['6:00 AM',	'7:00 AM',	'8:00 AM',	'9:00 AM',	'10:00 AM',	'11:00 AM',	'12:00 AM',	'1:00 AM',	'2:00 PM',	'3:00 PM',	'4:00 PM',	'5:00 PM',	'6:00 PM',	'7:00 PM',	'8:00 PM'];
+function place(min,max,location,average,hoursOfOperation){
   this.min = min,
-  this.max = max,
-  this.id = id,
-  this.average = average,
-  this.place = place,
-  this.hoursOfOperation = hoursOfOperation,
-  this.arrayOfAverageNumberOfPeople = [],
-  this.cookiesPerHour = [],
+  this.max = max;
+  this.average = average;
+  this.location = location;
+  this.hoursOfOperation = hoursOfOperation;
+  this.arrayOfAverageNumberOfPeople = [];
+  this.cookiesPerHour = [];
+  this.hourOfTheDay = [];
 
   this.calculateAverageNumberOfPeople = function (){
       for(var i = 0; i < this.hoursOfOperation; i++){
-      var randNum = Math.round(Math.random() * (max - min) + min);
-      this.arrayOfAverageNumberOfPeople.push(randNum);
+        var randNum = Math.round(Math.random() * (max - min) + min);
+        this.arrayOfAverageNumberOfPeople.push(randNum);
     }
+    console.log(this.arrayOfAverageNumberOfPeople);
   };
   this.calculateCookiesPerHour = function (){
     for(var i = 0; i < this.hoursOfOperation;i++){
       var cookiesThisHour = Math.round(this.arrayOfAverageNumberOfPeople[i] * this.average);
       this.cookiesPerHour.push(cookiesThisHour);
     } 
-  }
+    console.log(this.cookiesPerHour);
+  };
+  this.populateHoursOfTheDay = function(){
+    for(var i = 0;i < this.hoursOfOperation;i++){
+      var suffix = 'AM';
+      if(i > 7){
+        suffix ='PM'
+      }
+      this.hourOfTheDay.push(`${((i + 5) % 12) + 1 }:00 ${suffix}`);
+    }
+    console.log(this.hourOfTheDay);
+  };
+  this.render = function(){
+    this.populateHoursOfTheDay();
+    this.calculateAverageNumberOfPeople();
+    this.calculateCookiesPerHour();
+    var tableDataElement = document.getElementById('data');
+    var trElement = document.createElement('tr');
+    var tdEl = document.createElement('td');
+    tdEl.textContent = this.location;
+    trElement.appendChild(tdEl);
+
+    for(var i = 0; i < this.cookiesPerHour.length; i++){
+      var tdElement = document.createElement('td');
+      tdElement.textContent = this.cookiesPerHour[i];
+      trElement.appendChild(tdElement);
+    }
+    tableDataElement.appendChild(trElement);
+  };
+  this.render();
 }
-var place1stAndPike = new place1standPike(23,65,'1st and Pike', 6.3, 15,"calculatedCookiesFor1stAndPike")
+function createTableHead(){
+  var tableHeadElement = document.getElementById('head');
+  var trElement = document.createElement('tr');
+  var thEl = document.createElement('th');
+  trElement.appendChild(thEl);
+  for(var i =0; i < hours.length; i++){
+    var thElement = document.createElement('th');
+    thElement.textContent = hours[i];
+    trElement.appendChild(thElement);
+  }
+  tableHeadElement.appendChild(trElement);
+}
+
+var place1stAndPike = new place(23,65,'1st and Pike', 6.3, 15);
+var placeSeaTacAirport = new place(3,24,'SeaTac Airport', 1.2, 15);
+var placeSeattleCenter = new place(11,38,'Seattle Center',3.7, 15);
+var placeCapitolHill = new place(20,38,'Capitol Hill', 2.3,15);
+var placeAlki = new place(2,16,'Alki', 4.6, 15);
+createTableHead();
