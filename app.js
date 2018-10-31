73a3f9a27308ce9eb4eb0f164307dca0f6aa9dcb
Dan Le <Danhuyle508@gmail.com>
2018-10-30 21:16:01 -0700

    table is formatted with CSS

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var stores = [];
-var hours =['6:00 AM',	'7:00 AM',	'8:00 AM',	'9:00 AM',	'10:00 AM',	'11:00 AM',	'12:00 AM',	'1:00 AM',	'2:00 PM',	'3:00 PM',	'4:00 PM',	'5:00 PM',	'6:00 PM',	'7:00 PM',	'8:00 PM'];
+var hours =['6:00 AM',	'7:00 AM',	'8:00 AM',	'9:00 AM',	'10:00 AM',	'11:00 AM',	'12:00 AM',	'1:00 AM',	'2:00 PM',	'3:00 PM',	'4:00 PM',	'5:00 PM',	'6:00 PM',	'7:00 PM'];
+var totalOfTheHour = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
 function place(min,max,location,average,hoursOfOperation){
   this.min = min,
   this.max = max;
@@ -21,6 +23,7 @@ function place(min,max,location,average,hoursOfOperation){
     for(var i = 0; i < this.hoursOfOperation;i++){
       var cookiesThisHour = Math.round(this.arrayOfAverageNumberOfPeople[i] * this.average);
       this.cookiesPerHour.push(cookiesThisHour);
+      totalOfTheHour[i] += cookiesThisHour;
     } 
     console.log(this.cookiesPerHour);
   };
@@ -65,10 +68,23 @@ function createTableHead(){
   }
   tableHeadElement.appendChild(trElement);
 }
-
-var place1stAndPike = new place(23,65,'1st and Pike', 6.3, 15);
-var placeSeaTacAirport = new place(3,24,'SeaTac Airport', 1.2, 15);
-var placeSeattleCenter = new place(11,38,'Seattle Center',3.7, 15);
-var placeCapitolHill = new place(20,38,'Capitol Hill', 2.3,15);
-var placeAlki = new place(2,16,'Alki', 4.6, 15);
+function createTableFooter(){
+  var tableFooterElement = document.getElementById('footer');
+  var trElement = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = 'Total';
+  trElement.appendChild(tdEl);
+  for(var i =0; i < 14; i++){
+    var tdElement = document.createElement('td');
+    tdElement.textContent = totalOfTheHour[i];
+    trElement.appendChild(tdElement);
+  }
+  tableFooterElement.appendChild(trElement);
+}
+var place1stAndPike = new place(23,65,'1st and Pike', 6.3, 14);
+var placeSeaTacAirport = new place(3,24,'SeaTac Airport', 1.2, 14);
+var placeSeattleCenter = new place(11,38,'Seattle Center',3.7, 14);
+var placeCapitolHill = new place(20,38,'Capitol Hill', 2.3,14);
+var placeAlki = new place(2,16,'Alki', 4.6, 14);
 createTableHead();
+createTableFooter();
